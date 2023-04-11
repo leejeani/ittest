6787cddbded6ff4104ef3031061407c616b9e346
leejeani <jeanman.lee@gmail.com>
2023-04-11 15:28:29 +0900

    Update Main.java
    dd

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-
+        int b = 100;
         int a = 10;
         System.out.println("Hello world!");
     }
